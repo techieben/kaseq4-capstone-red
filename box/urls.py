d886555629bb3f9aaa3072327d86262b5f6e85c6
Benjamin Mark <benjmark@protonmail.com>
2020-07-10 16:49:42 -0400

    templates and static_dev directories added, views & urls updated

--- box/urls.py
+++ box/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 from recpie.urls import urlpatterns as recpie_urls
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
 ]
 
--- recpie/urls.py
+++ recpie/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from recpie import views
 
 urlpatterns = [
-    path('', views.index, name="homepage"),
+    # path('', views.index, name="homepage"),
     path('add_recpie/', views.add_recpie, name='add_recpie'),
     path('add_author/', views.add_author, name='add_author'),
     path('author/<int:id>/', views.author, name='author'),
