93262f6bcfe6ce7102fed5c3c04a0de82706840b
Benjamin Mark <benjmark@protonmail.com>
2020-07-17 14:31:44 -0400

    removed comments from index view

--- box/views.py
+++ box/views.py
@@ -1,15 +1,8 @@
 from recipe.models import Recipe
-# from django.views.generic import ListView
 from django.shortcuts import render
 
 
-# class IndexView(ListView):
-#     template_name = 'index.html'
-#     model = Recipe
-#     queryset = Recipe.objects.all().order_by('-date_created')[:20]
-
 def IndexView(request):
     html = 'index.html'
-    # recipes = Recipe.objects.all().order_by('-date_created')[:10]
     recipes = Recipe.objects.all()
     return render(request, html, {'recipes': recipes})
