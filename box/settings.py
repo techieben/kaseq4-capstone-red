e02e3320512fb89891e5a390026e01ef22269559
cbw29512 <divclass01@gmail.com>
2020-07-20 15:23:54 -0400

    moved secret key to /box/env file

--- box/settings.py
+++ box/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p5+r*p4hx@f*o&*nnl*bwrqi71rvubxjel_4z7iu+@te(!5dy7'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
